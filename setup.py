98d73baebd024cd2440f7207dd2f7186b3798cfc
alejandrovivas <alejo.vivas.av@gmail.com>
2021-12-14 16:16:13 -0500

    Upload files

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# django-sslmiddleware
+#
+#
+
+"""
+Install script for SSL Middleware
+"""
+
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='django-sslmiddleware',
+    version='1.7',
+    description="",
+    license='Python',
+    url='http://www.djangosnippets.org/snippets/85/',
+    packages = [ '', ],
+    package_dir = { '': 'src' },
+    include_package_data = True,
+    zip_safe = False,
+)
